3f24034b862e655fc82fc86682781424de93ca82
Hemil08 <hemilsorathiya008@gmail.com>
2026-04-22 12:14:33 +0200

    Add dotenv configuration and Google OAuth strategy to server setup

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import express from "express"
+import { config } from "dotenv"
+import { Strategy as GoogleStrategy } from "passport-google-oauth20"
 
+
+config()
 
 const app = express()
+
+app.get('/', (req,res) => {
+    res.send("Hello World")
+} )
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000")
